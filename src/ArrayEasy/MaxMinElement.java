e1ecf005325535ef0c4345e9628be43dc9c7a225
2019pgcaca07 <ranjeetkccc12@gmail.com>
2024-02-20 13:02:51 +0530

    1st program

--- src/ArrayEasy/MaxMinElement.java
+++ src/ArrayEasy/MaxMinElement.java
@@ -1,9 +1,11 @@
 package ArrayEasy;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MaxMinElement {
     public static void main(String []args){
+        //Given array
         int[] arr = {3,5,4,1,9};
         Scanner input = new Scanner(System.in);
 
@@ -18,10 +20,11 @@ public class MaxMinElement {
                 function2(arr);
                 break;
             default:
-                boolean Invalid;
+
                 System.out.println("WRONG CHOICE");
         }
     }
+    //this gunction taking o(n) as using linear search
     private static void function1(int[] arr){
            int min = Integer.MAX_VALUE;
            int max = Integer.MIN_VALUE;
@@ -36,6 +39,8 @@ public class MaxMinElement {
            System.out.println("min is "+min +" max is "+ max);
     }
     private static void function2(int[] arr){
-         System.out.println("2 choice");
+         //using java inbuilt sort method to solve this in o(nlogn)
+        Arrays.sort(arr);
+        System.out.println("min is "+arr[0]+" and max is "+arr[arr.length-1]);
     }
 }
